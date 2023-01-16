a6dfb68cd5a12d717e18f2daea788ccbd2221818
Aniket Korwar <78900271+aniketh1@users.noreply.github.com>
2023-01-16 10:45:24 +0530

    Update Finalize.java

--- src/Finalize.java
+++ src/Finalize.java
@@ -5,7 +5,7 @@ public class Finalize {
     public static void main(String args[]){
         Finalize f = new Finalize();
         f = null;
-        System.gc();
+        System.gc()
         System.out.println("it is in main method");
     }
 }
