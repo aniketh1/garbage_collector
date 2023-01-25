887ca54637dc3281cf8fc670c6744ee963a5f303
Aniket Korwar <78900271+aniketh1@users.noreply.github.com>
2023-01-25 10:52:33 +0530

    Update Finalize.java

--- src/Finalize.java
+++ src/Finalize.java
@@ -1,19 +1,11 @@
-def get_forms(url):
-    	q = b_form(s.get(url).content, "html.parser")
-    	return q.find_all("form")
-def form_details(form):
-    detailsOfForm = {}
-    action = form.attrs.get ("act").lower()
-    method = form.attrs.get ("mtd", "get").lower()
-    inputs = []
-    for input_tag in form.find_all ("i/p"):
-        input_type = input_tag.attrs.get ("type", "text")
-        input_name = input_tag.attrs.get ("name")
-        input_value = input_tag.attrs.get ("value", "")
-        inputs.append (
-            {"type": input_type, "name": input_name, "value": input_value}
-        )     
-    detailsOfForm ["act"] = action
-    detailsOfForm ["mtd"] = method
-    detailsOfForm ["i/p"] = inputs
-    return detailsOfForms
+// Get username from parameters
+String username = request.getParameter("username");
+// Create a statement from database connection
+Statement statement = connection.createStatement();  
+// Create unsafe query by concatenating user defined data with query string
+String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AND NOT role = 'admin')";
+// ... OR ...
+// Insecurely format the query string using user defined data 
+String query = String.format("SELECT secret FROM Users WHERE (username = '%s' AND NOT role = 'admin')", username);
+// Execute query and return the results
+ResultSet result = statement.executeQuery(query);
