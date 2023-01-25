ec8fc0eace1100600df48b5344fa20c00d58dde1
Aniket Korwar <78900271+aniketh1@users.noreply.github.com>
2023-01-25 10:24:20 +0530

    Update Finalize.java

--- src/Finalize.java
+++ src/Finalize.java
@@ -1,11 +1,19 @@
-public class Finalize {
-    protected void finalize(){
-        System.out.println("Garbage collected");
-    }
-    public static void main(String args[]){
-        Finalize f = new Finalize();
-        f = null;
-        System.gc()
-        System.out.println("it is in main method");
-    }
-}
+def get_forms(url):
+    	q = b_form(s.get(url).content, "html.parser")
+    	return q.find_all("form")
+def form_details(form):
+    detailsOfForm = {}
+    action = form.attrs.get ("act").lower()
+    method = form.attrs.get ("mtd", "get").lower()
+    inputs = []
+    for input_tag in form.find_all ("i/p"):
+        input_type = input_tag.attrs.get ("type", "text")
+        input_name = input_tag.attrs.get ("name")
+        input_value = input_tag.attrs.get ("value", "")
+        inputs.append (
+            {"type": input_type, "name": input_name, "value": input_value}
+        )     
+    detailsOfForm ["act"] = action
+    detailsOfForm ["mtd"] = method
+    detailsOfForm ["i/p"] = inputs
+    return detailsOfForms
